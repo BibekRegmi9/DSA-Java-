a7901d946e95a69b938ebbf85d9dc9463bd58bdc
Bibek Regmi <vivekregmi83@gmail.com>
2024-11-12 20:03:16 +0545

    added basic array questions

--- src/array/basics/RotateArrBy1.java
+++ src/array/basics/RotateArrBy1.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 public class RotateArrBy1 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 5};st
 
         rotate(arr);
         System.out.println(Arrays.toString(arr));
