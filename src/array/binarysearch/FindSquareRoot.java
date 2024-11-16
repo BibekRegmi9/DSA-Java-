33dd995047eb45ffef1707607e709486d5603d78
Bibek Regmi <vivekregmi83@gmail.com>
2024-11-16 18:56:59 +0545

    fix: sqrt problem

--- src/array/binarysearch/FindSquareRoot.java
+++ src/array/binarysearch/FindSquareRoot.java
@@ -7,6 +7,13 @@ public class FindSquareRoot {
     }
 
     public static int mySqrt(int x) {
+        if(x == 0){
+            return 0;
+        }
+        if(x == 1){
+            return 1;
+        }
+
         int start = 0;
         int end = x;
 
