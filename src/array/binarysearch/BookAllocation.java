f4d3d411d9dc732881d7a88cfbb9f49c7969b0da
Bibek Regmi <vivekregmi83@gmail.com>
2024-11-18 22:45:58 +0545

    add

--- src/array/binarysearch/BookAllocation.java
+++ src/array/binarysearch/BookAllocation.java
@@ -5,7 +5,47 @@ import java.util.Collections;
 
 public class BookAllocation {
     public static void main(String[] args) {
+        int[] arr = {12, 34, 67, 90};
+        int ans = bookAllocation(arr, 2);
+        System.out.println(ans);
     }
+
+    public static int bookAllocation(int[] arr, int k){
+
+        if(k > arr.length){
+            return -1;
+        }
+
+        int start = Arrays.stream(arr).max().getAsInt();
+        int end = Arrays.stream(arr).sum();
+        int ans = -1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            int page = 0;
+            int count = 1;
+
+            for(int i = 0; i < arr.length; i++){
+                page += arr[i];
+
+                if(page > mid){
+                    count++;
+                    page = arr[i];
+                }
+            }
+
+            if(count <= k){
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+
+
 
     public static int findPages(int[]A,int N,int M)
     {
--- src/array/binarysearch/KthMissingPositiveNumber.java
+++ src/array/binarysearch/KthMissingPositiveNumber.java
@@ -14,4 +14,23 @@ public class KthMissingPositiveNumber {
         }
         return k;
     }
+
+    public static int findKthMissingPositive(int[] arr, int k) {
+
+        int start = 0;
+        int end = arr.length-1;
+        int ans = arr.length;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if((arr[mid] - mid - 1) >= k){
+                end = mid - 1;
+                ans = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return ans + k;
+    }
 }
