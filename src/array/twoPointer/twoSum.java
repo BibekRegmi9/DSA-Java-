e1d4d47241914a22d646e089945564222cf33a02
Bibek Regmi <vivekregmi83@gmail.com>
2024-12-01 20:54:58 +0545

    two sum bruteforce

--- src/array/twoPointer/twoSum.java
+++ src/array/twoPointer/twoSum.java
@@ -1,7 +1,16 @@
 package array.twoPointer;
 
+import java.util.Arrays;
+
 public class twoSum {
-    public int[] twoSum(int[] numbers, int target) {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4};
+        int target = 6;
+//        int[] ans = twoSum(arr, target);
+        int[] ans = twoSumBruteforce(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int[] twoSum(int[] numbers, int target) {
         int start = 0;
         int end = numbers.length-1;
 
@@ -12,6 +21,17 @@ public class twoSum {
                 start++;
             } else {
                 end--;
+            }
+        }
+        return new int[] {-1, -1};
+    }
+
+    public static int[] twoSumBruteforce(int[] arr, int target){
+        for(int i = 0; i < arr.length-1; i++){
+            for(int j = 1; j < arr.length; j++){
+                if(arr[i] + arr[j] == target){
+                    return new int[] {i, j};
+                }
             }
         }
         return new int[] {-1, -1};
